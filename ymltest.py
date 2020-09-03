0e1507e0522ccb75b1f27b9a23f695d655e94e85
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 21:01:08 +0200

    tries

--- ymltest.py
+++ ymltest.py
@@ -1,4 +1,5 @@
 import yaml
+import mysql.connector
 
 with open("test.yml", "r") as s:
     y = yaml.load(s)
@@ -8,12 +9,11 @@ with open("test.yml", "r") as s:
 
 min_password_length: y["min-password-length"]
 data_save_type = y["data-save-type"]
+mysql_settings = y["mysql"]
+sqlite_settings = y["sqlite"]
 
 ##################################################
 
-
-
-"""
 min_password_length = y["min-password-length"]
 data_save_type = y["data-save-type"]
 
@@ -22,8 +22,22 @@ print("-------------------")
 
 if data_save_type == "SQLITE":
     print("SQLITE MODE")
+    print(sqlite_settings)
 elif data_save_type == "MYSQL":
     print("MYSQL MODE")
+    print(mysql_settings)
 else:
     print("Data save mode unreadable")
-"""
+
+databasemysql = mysql.connector.connect(
+  host= mysql_settings["host"],
+  user= mysql_settings["user"],
+  password=mysql_settings["password"],
+  database=mysql_settings["database"]
+)
+
+print(mysql_settings["host"])
+
+c = databasemysql.cursor()
+
+c.execute("SELECT username FROM userdata WHERE username = %s", ("Csiga1111",))
